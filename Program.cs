b62c693cb96bd73766501abee40b95812a9f0941
jorda <jorda@LAPTOP-4BQV6VKK>
2024-04-23 21:12:40 +0200

    Define main class of Space Shuttle Assessment

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace SpaceShuttle
+{ 
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Welcome to the Space Shuttle Programme!");
+            Console.WriteLine("Made by Viktor Yordanov");
+            Console.WriteLine("Please input the full path of a .csv file that contains the weather report for the monty of July:");
+            string inputPath = Console.ReadLine();
+
+            if(!File.Exists(inputPath) || Path.GetExtension(inputPath) != ".csv")
+            {
+                Console.WriteLine("This is not a valid path!");
+                return;
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
+
