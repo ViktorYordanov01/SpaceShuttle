a6371c912ce974efbfed617996dddba56f95afd1
jorda <jorda@LAPTOP-4BQV6VKK>
2024-04-25 10:52:07 +0200

    Add two classes that read and write into .csv file

---
+++ ReadCsvFile.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpaceLaunch
+{
+    class ReadCsvFile
+    {
+        private string filePath;
+        private TextFieldParser parser;
+        private List<List<string>> extractedRawDataColumns;
+        public List<List<string>> GetRawDataColumns => extractedRawDataColumns;
+        private List<List<string>> extractedRawDataRows;
+        public List<List<string>> GetRawDataRows => extractedRawDataRows;
+        public ReadCsvFile(string filePath)
+        {
+            this.filePath = filePath; 
+
+            ExtractData(filePath);
+
+        }
+
+        private void ExtractData(string filePath)
+        {
+            this.parser = new TextFieldParser(filePath);
+            this.parser.TextFieldType = FieldType.Delimited;
+            this.parser.SetDelimiters(",");
+            extractedRawDataColumns = new List<List<string>>();
+            extractedRawDataRows = new List<List<string>>();
+
+            string[] days = parser.ReadFields();
+            int numberOfColumns = days.Length;
+            extractedRawDataRows.Add(days.ToList());
+
+            for (int i = 0; i < numberOfColumns; i++)
+            {
+                extractedRawDataColumns.Add(new List<string>());
+            }
+            for (int i = 0; i < numberOfColumns; i++)
+            {
+                extractedRawDataColumns[i].Add(days[i]);
+            }
+
+            while (!parser.EndOfData)
+            {
+                string[] row = parser.ReadFields();
+                extractedRawDataRows.Add(row.ToList());
+                for (int i = 0; i < numberOfColumns; i++)
+                {
+                    extractedRawDataColumns[i].Add(row[i]);
+                }
+            }
+
+            parser.Close();
+        }
+        public void printData()
+        {
+            for (int i = 0; i < extractedRawDataRows.Count; i++)
+            {
+                for (int j = 0; j < extractedRawDataRows[i].Count; j++)
+                {
+                    Console.Write(extractedRawDataRows[i][j] + ",");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        public bool ValidateFieldNames()
+        {
+            if (extractedRawDataColumns[0].Count != 7) return false;
+            if (extractedRawDataColumns[0][0] != "Day") return false;
+            if (extractedRawDataColumns[0][1] != "Temperature") return false;
+            if (extractedRawDataColumns[0][2] != "Wind") return false;
+            if (extractedRawDataColumns[0][3] != "Humidity") return false;
+            if (extractedRawDataColumns[0][4] != "Precipitation") return false;
+            if (extractedRawDataColumns[0][5] != "Lightning") return false;
+            if (extractedRawDataColumns[0][6] != "Clouds") return false;
+
+            return true;
+        }
+
+    }
+}
---
+++ WriteCsvFile.cs
@@ -0,0 +1,87 @@
+﻿using SpaceShuttle;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace SpaceLaunch
+{
+    class WriteCsvFile
+    {
+        StreamWriter writer;
+        public string FilePath { get; set; } = "";
+        public WriteCsvFile(List<List<string>> rawData, string filePath)
+        {
+            writer = new StreamWriter(filePath);
+            FilePath = filePath;
+            writer.WriteLine("Parameters,AverageValue,MinValue,MaxValue,MedianValue,BestLaunchDate");
+            WriteSummarizedData(rawData);
+        }
+
+        private void WriteNumberData(List<string> data, int lowerBound, int upperBound, string name)
+        {
+            List<string> removeName = data.GetRange(1, data.Count - 1);
+            List<int> convertedValues;
+            try
+            {
+                convertedValues = removeName.Select(int.Parse).ToList();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Input data was not in the correct format");
+                return;
+            }
+            SummaryNumField numberData = new SummaryNumField(convertedValues, name, lowerBound, upperBound);
+            writer.WriteLine(name + "," + numberData.AverageValue + "," + numberData.MinimalValue + "," + numberData.MaximumValue + "," +
+                             numberData.MedianValue + "," + numberData.BestLaunchDay);
+        }
+        private void WriteStringData(List<string> data, string name)
+        {
+            SummaryStrFields stringData = new SummaryStrFields(data.GetRange(1, data.Count - 1), name);
+            writer.WriteLine(name + ",,,,," + stringData.BestLaunchDate);
+        }
+        private void WriteSummarizedData(List<List<string>> rawData)
+        {
+            foreach (List<string> l in rawData)
+            {
+                string name = l[0];
+                switch (name)
+                {
+                    case "Temperature":
+                        {
+                            WriteNumberData(l, 2, 31, name);
+                        }
+                        break;
+                    case "Wind":
+                        {
+                            WriteNumberData(l, 0, 11, name);
+                        }
+                        break;
+                    case "Humidity":
+                        {
+                            WriteNumberData(l, 0, 60, name);
+                        }
+                        break;
+                    case "Precipitation":
+                        {
+                            WriteNumberData(l, 0, 0, name);
+                        }
+                        break;
+                    case "Lightning":
+                        {
+                            WriteStringData(l, name);
+                        }
+                        break;
+                    case "Clouds":
+                        {
+                            WriteStringData(l, name);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            writer.Close();
+        }
+    }
+}
