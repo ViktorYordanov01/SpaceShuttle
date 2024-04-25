bbe0130801260da765f9a929d23e55c6207bd09c
jorda <jorda@LAPTOP-4BQV6VKK>
2024-04-25 13:44:14 +0200

    Add EmailSender and AnalyzeData classes

---
+++ AnalyzeData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpaceLaunch
+{
+    class AnalyzeData
+    {
+        List<SingleDay> dayEntries;
+
+        public AnalyzeData(List<List<string>> rawData)
+        {
+            dayEntries = new List<SingleDay>();
+            foreach (List<string> item in rawData)
+            {
+                SingleDay tmpSingleDay = new SingleDay(item);
+                if (tmpSingleDay.ValidData) { dayEntries.Add(tmpSingleDay); }
+            }
+        }
+
+        public int CalculateBestLaunchDay()
+        {
+            SingleDay currentBestDay = dayEntries[0];
+            foreach (SingleDay dayEntry in dayEntries)
+            {
+                if (CompareDays(currentBestDay, dayEntry))
+                {
+                    currentBestDay = dayEntry;
+                }
+            }
+
+            if (currentBestDay.ValidLaunchDate) return currentBestDay.Date;
+            return 0;
+        }
+
+        private bool CompareDays(SingleDay currentBest, SingleDay currentDay)
+        {
+            if (currentDay.ValidLaunchDate)
+            {
+                if (!currentBest.ValidData) 
+                    return true;
+
+                if (currentDay.WindSpeed < currentBest.WindSpeed) 
+                    return true;
+                else if (currentDay.WindSpeed == currentBest.WindSpeed && currentDay.Humidity < currentBest.Humidity) 
+                    return true;
+
+                return false;
+            }
+            return false;
+        }
+    }
+}
---
+++ EmailSender.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace SpaceShuttle
+{
+    class EmailSender
+    {
+        public string SenderAddress { get; set; } = "";
+        public string SenderPswd { get; set; } = "";
+        public string ReceiverAddress { get; set; } = "";
+        public string FilePath { get; set; } = "";
+        public int BestLaunchDate { get; set; } = 0;
+        public EmailSender(string senderAddress, string senderPswd, string receiverAddres, string filePath, int bestLaunchDate)
+        {
+            SenderAddress = senderAddress;
+            SenderPswd = senderPswd;
+            ReceiverAddress = receiverAddres;
+            FilePath = filePath;
+            BestLaunchDate = bestLaunchDate;
+        }
+
+        public void Send()
+        {
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(SenderAddress);
+            mailMessage.To.Add(new MailAddress(ReceiverAddress));
+            mailMessage.Subject = "Weather report for rocket launch!";
+            mailMessage.Body = "Best Launch day is : " + BestLaunchDate.ToString();
+
+            Attachment attachment = new Attachment(FilePath);
+            mailMessage.Attachments.Add(attachment);
+
+            try
+            {
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(SenderAddress, SenderPswd);
+                smtp.Send(mailMessage);
+                Console.WriteLine("Email Sent!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error while sending the email!");
+                return;
+            }
+        }
+    }
+}
